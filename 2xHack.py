d4f8540e5d4fe113dbbe192cdc498d2a4e272ae4
Pradeep Lodhi <115186297+pradeep2x@users.noreply.github.com>
2022-10-07 22:27:41 +0530

    2xHack

--- 2xHack.py
+++ 2xHack.py
@@ -10,7 +10,7 @@ def main():
    print '================================================='
    print '               ++++++++++++++++++++              '
    print '\n                                               '
-   print '                °°°°°°°°°°°°°°°°°°               '
+   print '                                                 '
    print '                                                 '
    print '                                                 '
    print '          Pradeep2x                '             '
